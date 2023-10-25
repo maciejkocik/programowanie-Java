5d000fa134846d2a3c7ba12d782b5e8875e1b6c4
Maciej Kocik <maciejkocik04@gmail.com>
2023-10-25 11:35:24 +0200

    silnia

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    private static int silnia(int x){
+        if (x >= 1){
+            int wynik = 1;
+            for (int i = 2; i <= x; ++i) {
+                wynik *= i;
+            }
+            return wynik;
+        } else return 1;
     }
+
+    public static void main(String[] args) {
+
+        int a = 6;
+        System.out.println(silnia(a));
+        /*
+
+        for (int i = 0; i < 10; ++i){
+            System.out.println(i);
+        }
+
+        int x = 5;
+
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.println();
+        x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        /*
+        double d = 3.14;
+        boolean b = true;
+
+        Integer y = 42;
+        Double dd = 4.15;
+        Boolean bb = false;
+
+        String s = "1138";
+        int z = Integer.parseInt(s);
+
+        System.out.println("The length of string s is " + s.length());
+        System.out.println("Hello user number " + (z * 10));
+        */
+
+    }
+
 }
